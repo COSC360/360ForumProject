dd1fe030a3642453992f14846b331e1b16a51c00
Bhavya <bhavya25@students.cs.ubc.ca>
2023-03-26 23:26:28 -0700

    ajax

--- profile.php
+++ profile.php
@@ -14,5 +14,14 @@ if(!isset($_SESSION['email']))
 <body>
 <h2>Welcome <?php echo $_SESSION['email']; ?></h2>
 <a href="logout.php">Logout</a>
+<span></span>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+        $(document).ready(function(){
+            setInterval(function(){
+                $(span).load('login.html')
+            }, 2000);
+        });
+    </script>
 </body>
 </html>
